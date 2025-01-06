c86b52b867c0650dd369fb4da9625a13de07460d
Nancy Scott <scot0854@umn.edu>
2025-01-06 10:07:20 -0600

    Add serial timeline plot, update renv, apply styler addin

--- MIC_growth_curve/archive/MIC_SMG_plots_redcap.R
+++ MIC_growth_curve/archive/MIC_SMG_plots_redcap.R
@@ -4,7 +4,7 @@
 ## Email: scot0854@umn.edu
 ## ---------------------------
 ## Notes:
-## OD readings are read from one sheet and pivoted to "long" format, 
+## OD readings are read from one sheet and pivoted to "long" format,
 ## metadata in long format is read from another sheet and concatenated
 ## ---------------------------
 options(scipen = 999)
@@ -19,16 +19,16 @@ source("MIC_growth_curve/MIC_heatmap.R")
 
 # VARIABLES - READ THESE CAREFULLY!!!!!!!!!!!!!!!!!!!!!!
 # For uploading to REDcap
-api_url <-  "https://redcap.ahc.umn.edu/redcap/api/"
+api_url <- "https://redcap.ahc.umn.edu/redcap/api/"
 
 # Enter drug and spreadsheet data
 input_drug <- "flc"
 replicates <- 3
 
-mic_spreadsheet <-""
+mic_spreadsheet <- ""
 smg_spreadsheet <- ""
 
-od_tab <- 1  # Excel tab of OD values
+od_tab <- 1 # Excel tab of OD values
 metadata_tab <- 2 # Excel tab of metadata
 metadata_range <- "A1:F13"
 
@@ -39,8 +39,10 @@ column_names <- "concentration"
 cols_used <- 12
 
 # Drug concentrations (this assumes the usual "screening" set-up, CHANGE AS NEEDED)
-concentration <- case_when(toupper(input_drug) == "FLC" ~ c(0,0.5,1,2,4,8,16,32),
-                           toupper(input_drug) %in% c("MCF","AMB") ~ c(0,0.016,0.032,0.064,0.125,0.256,0.5,1))
+concentration <- case_when(
+  toupper(input_drug) == "FLC" ~ c(0, 0.5, 1, 2, 4, 8, 16, 32),
+  toupper(input_drug) %in% c("MCF", "AMB") ~ c(0, 0.016, 0.032, 0.064, 0.125, 0.256, 0.5, 1)
+)
 
 max_concentration <- 32
 
@@ -48,34 +50,41 @@ max_concentration <- 32
 control_strains <- c("AMS5123", "AMS5122")
 
 # FOR SPECIFIC ORDER: put IDs in "strains" vector. Otherwise leave commented out.
-#strain_list <- c("AMS5123", "MEC257", "MEC208", "MEC205", "MEC194", "MEC190", "MEC188", "MEC176", "MEC175")
+# strain_list <- c("AMS5123", "MEC257", "MEC208", "MEC205", "MEC194", "MEC190", "MEC188", "MEC176", "MEC175")
 
-# TO SKIP STRAINS IN PLOT: 
+# TO SKIP STRAINS IN PLOT:
 # Need strain_list above with wanted strains plus IDs to skip in "exclude" vector. Otherwise leave commented out.
-#exclude <- c("MEC174", "MEC185", "MEC196")
+# exclude <- c("MEC174", "MEC185", "MEC196")
 
-mic_date <-str_extract(mic_spreadsheet, "\\d+-\\d+-\\d+")
+mic_date <- str_extract(mic_spreadsheet, "\\d+-\\d+-\\d+")
 
 ################################################################################
 # Load metadata
-meta.frame <- read_excel(mic_spreadsheet, 
-                         sheet = metadata_tab,
-                         range = metadata_range)
+meta.frame <- read_excel(mic_spreadsheet,
+  sheet = metadata_tab,
+  range = metadata_range
+)
 
-meta.frame$drug <- c(toupper(input_drug), rep(NA, times=cols_used - length(input_drug)))
+meta.frame$drug <- c(toupper(input_drug), rep(NA, times = cols_used - length(input_drug)))
 
-meta.frame$concentration <- c(concentration, rep(NA, times=cols_used - length(concentration)))
+meta.frame$concentration <- c(concentration, rep(NA, times = cols_used - length(concentration)))
 
 # Set vars for use below
-meta_names = case_when(column_names=="strain" ~ "strain",
-                       column_names=="concentration" ~ "concentration")
-meta_col = case_when(column_names!="strain" ~ "strain",
-                     column_names !="concentration" ~ "concentration")
-    
-cutoff <- case_when(meta.frame$drug[1] %in% c("FLC", "MCF") ~ 0.5,
-                    meta.frame$drug[1] == "AMB" ~ 0.1)
+meta_names <- case_when(
+  column_names == "strain" ~ "strain",
+  column_names == "concentration" ~ "concentration"
+)
+meta_col <- case_when(
+  column_names != "strain" ~ "strain",
+  column_names != "concentration" ~ "concentration"
+)
 
-#x_axis_angle <- case_when(meta.frame$drug[1]== "FLC" ~ 0,
+cutoff <- case_when(
+  meta.frame$drug[1] %in% c("FLC", "MCF") ~ 0.5,
+  meta.frame$drug[1] == "AMB" ~ 0.1
+)
+
+# x_axis_angle <- case_when(meta.frame$drug[1]== "FLC" ~ 0,
 #                          meta.frame$drug[1] == "MCF" ~ 90,
 #                          meta.frame$drug[1] == "AMB" ~ 90)
 x_axis_angle <- 0
@@ -83,20 +92,23 @@ x_axis_angle <- 0
 ################################################################################
 # MIC
 # Loop over each plate
-for(i in 1:replicates){
-  d.frame <- read_excel(mic_spreadsheet, 
-                        sheet = od_tab,
-                       range = meta.frame[[tolower(input_drug)]][i])
-  
-  # Colnames set from input variable 
-  colnames(d.frame) <- as.character(pull(meta.frame,column_names))
-  
+for (i in 1:replicates) {
+  d.frame <- read_excel(mic_spreadsheet,
+    sheet = od_tab,
+    range = meta.frame[[tolower(input_drug)]][i]
+  )
+
+  # Colnames set from input variable
+  colnames(d.frame) <- as.character(pull(meta.frame, column_names))
+
   # Add (strain or concentration) not used as column names as a new column
-  if(!all(meta.frame$strain[!is.na(meta.frame$strain)] %in% colnames(d.frame))) { 
-      d.frame$strain = meta.frame$strain[!is.na(meta.frame$strain)]} else { 
-          d.frame$concentration = as.character(meta.frame$concentration[!is.na(meta.frame$concentration)])}
-  
-  # Add remaining metadata 
+  if (!all(meta.frame$strain[!is.na(meta.frame$strain)] %in% colnames(d.frame))) {
+    d.frame$strain <- meta.frame$strain[!is.na(meta.frame$strain)]
+  } else {
+    d.frame$concentration <- as.character(meta.frame$concentration[!is.na(meta.frame$concentration)])
+  }
+
+  # Add remaining metadata
   d.frame <- d.frame %>%
     add_column(plate = i) %>%
     add_column(drug = meta.frame$drug[!is.na(meta.frame$drug)]) %>%
@@ -108,63 +120,71 @@ for(i in 1:replicates){
 
 # Pull together and tidy data
 drug_data <- bind_rows(mget(ls(pattern = "^rep\\d+$")))
-drug <- pivot_longer(drug_data, 
-                    names_to = meta_names,
-                    values_to = "OD600", 
-                    cols = -c(all_of(meta_col), plate, drug, media, temp)) 
+drug <- pivot_longer(drug_data,
+  names_to = meta_names,
+  values_to = "OD600",
+  cols = -c(all_of(meta_col), plate, drug, media, temp)
+)
 
 # BLANKS: this checks for "blank" labeled wells in metadata
 # if none found, sets it to the highest concentration well of the control strain
 drug <- drug %>%
-    mutate(concentration = case_when((!("blank" %in%  c(strain, concentration)) & concentration == as.character(max_concentration) & strain %in% control_strains) ~ "blank",
-                                     .default = concentration))
-drug$concentration <-factor(tolower(drug$concentration), levels = mixedsort(unique(drug$concentration))) 
+  mutate(concentration = case_when((!("blank" %in% c(strain, concentration)) & concentration == as.character(max_concentration) & strain %in% control_strains) ~ "blank",
+    .default = concentration
+  ))
+drug$concentration <- factor(tolower(drug$concentration), levels = mixedsort(unique(drug$concentration)))
 
-# Create strain list for plotting (alphanumeric + control at bottom) unless it's been entered above 
-if(exists("strain_list")) {
-    strains <- strain_list
+# Create strain list for plotting (alphanumeric + control at bottom) unless it's been entered above
+if (exists("strain_list")) {
+  strains <- strain_list
 } else {
-    strains <- c(unique(drug$strain[drug$strain %in% control_strains]), rev(unique(drug$strain[!(drug$strain %in% control_strains)])))}
+  strains <- c(unique(drug$strain[drug$strain %in% control_strains]), rev(unique(drug$strain[!(drug$strain %in% control_strains)])))
+}
 
 mic_boxplot(drug)
 ################################################################################
 # Read in SMG ODs and reuse metadata from MIC spreadsheet
-smg_range <- read_excel(smg_spreadsheet, sheet=metadata_tab)
-for(i in 1:replicates){
-    d.frame <- read_excel(smg_spreadsheet,
-                          sheet = od_tab,
-                          range = smg_range[[tolower(input_drug)]][i])
-    
-    # Colnames set from input variable 
-    colnames(d.frame) <- as.character(pull(meta.frame,column_names))
-    
-    # Add (strain or concentration) not used as column names as a new column
-    if(!all(colnames(d.frame) == meta.frame$strain)) { 
-        d.frame$strain = meta.frame$strain[!is.na(meta.frame$strain)]} else {
-             d.frame$concentration = as.character(meta.frame$concentration[!is.na(meta.frame$concentration)])}
-    
-    # Add remaining metadata 
-    d.frame <- d.frame %>%
-        add_column(plate = i) %>%
-        add_column(drug = meta.frame$drug[!is.na(meta.frame$drug)]) %>%
-        add_column(media = meta.frame$media[!is.na(meta.frame$media)]) %>%
-        add_column(temp = meta.frame$temp[!is.na(meta.frame$temp)])
-    
-    assign(paste("rep", i,sep = ""), d.frame)
+smg_range <- read_excel(smg_spreadsheet, sheet = metadata_tab)
+for (i in 1:replicates) {
+  d.frame <- read_excel(smg_spreadsheet,
+    sheet = od_tab,
+    range = smg_range[[tolower(input_drug)]][i]
+  )
+
+  # Colnames set from input variable
+  colnames(d.frame) <- as.character(pull(meta.frame, column_names))
+
+  # Add (strain or concentration) not used as column names as a new column
+  if (!all(colnames(d.frame) == meta.frame$strain)) {
+    d.frame$strain <- meta.frame$strain[!is.na(meta.frame$strain)]
+  } else {
+    d.frame$concentration <- as.character(meta.frame$concentration[!is.na(meta.frame$concentration)])
+  }
+
+  # Add remaining metadata
+  d.frame <- d.frame %>%
+    add_column(plate = i) %>%
+    add_column(drug = meta.frame$drug[!is.na(meta.frame$drug)]) %>%
+    add_column(media = meta.frame$media[!is.na(meta.frame$media)]) %>%
+    add_column(temp = meta.frame$temp[!is.na(meta.frame$temp)])
+
+  assign(paste("rep", i, sep = ""), d.frame)
 }
-   
+
 # Pull together and tidy data
 drug48_data <- bind_rows(mget(ls(pattern = "^rep\\d+$")))
-drug48 <- pivot_longer(drug48_data, 
-                     names_to = meta_names,
-                     values_to = "OD600", 
-                     cols = -c(all_of(meta_col), plate, drug, media, temp)) 
+drug48 <- pivot_longer(drug48_data,
+  names_to = meta_names,
+  values_to = "OD600",
+  cols = -c(all_of(meta_col), plate, drug, media, temp)
+)
 
 # This sets the blank when it's one well of the control strain
 drug48 <- drug48 %>%
-    mutate(concentration = case_when((!("blank" %in%  c(strain, concentration)) & concentration == as.character(max_concentration) & strain %in% control_strains) ~ "blank",
-                                     .default = concentration))
-drug48$concentration <-factor(tolower(drug48$concentration), levels = mixedsort(unique(drug48$concentration))) 
+  mutate(concentration = case_when((!("blank" %in% c(strain, concentration)) & concentration == as.character(max_concentration) & strain %in% control_strains) ~ "blank",
+    .default = concentration
+  ))
+drug48$concentration <- factor(tolower(drug48$concentration), levels = mixedsort(unique(drug48$concentration)))
 
 mic_boxplot(drug48)
 ################################################################################
@@ -185,36 +205,43 @@ drug_plotting_coords <- plotting_coords(drug_od, drug_mic, cutoff, strains)
 
 # Make the "heatmap"
 drug_mic_plot <- mic_plot(drug_od, strains, drug_plotting_coords) +
-        #xlab(case_when(drug$drug[1] %in% c("AMB", "MCF") ~ paste("\n", drug$drug[1]," MIC"),
-         #    .default = paste(drug$drug[1], " MIC"))) +
-        xlab(paste(drug$drug[1], " MIC")) +
-        theme(axis.text.x = element_text(angle = x_axis_angle),
-          plot.margin = unit(c(0,0.5,0.5,0.5), "cm"),
-          axis.title.y = element_text(hjust = 0.5,
-                                      margin = margin(0,10,0,0))) 
+  # xlab(case_when(drug$drug[1] %in% c("AMB", "MCF") ~ paste("\n", drug$drug[1]," MIC"),
+  #    .default = paste(drug$drug[1], " MIC"))) +
+  xlab(paste(drug$drug[1], " MIC")) +
+  theme(
+    axis.text.x = element_text(angle = x_axis_angle),
+    plot.margin = unit(c(0, 0.5, 0.5, 0.5), "cm"),
+    axis.title.y = element_text(
+      hjust = 0.5,
+      margin = margin(0, 10, 0, 0)
+    )
+  )
 
-# Barplot   
+# Barplot
 drug_smg_plot <- smg_plot(left_join(drug_mic, drug_smg), strains) +
-    theme(axis.text.y = element_blank(),
-          axis.text.x = element_text(),
-         plot.margin = unit(c(0,0.5,0.5,0), "cm"))+#,
-          #axis.title.x = element_text(lineheight = 1.1)) +
-    #xlab(case_when(drug$drug[1] %in% c("AMB", "MCF")~ paste("\n\n","SMG"),
-         #.default = "SMG")) 
-    xlab("SMG")
-    
+  theme(
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(),
+    plot.margin = unit(c(0, 0.5, 0.5, 0), "cm")
+  ) + # ,
+  # axis.title.x = element_text(lineheight = 1.1)) +
+  # xlab(case_when(drug$drug[1] %in% c("AMB", "MCF")~ paste("\n\n","SMG"),
+  # .default = "SMG"))
+  xlab("SMG")
+
 # Pull together with patchwork
-drug_full_plot <- (drug_mic_plot + drug_smg_plot) + plot_layout(guides = 'collect')
+drug_full_plot <- (drug_mic_plot + drug_smg_plot) + plot_layout(guides = "collect")
 
 # Save as desired
-ggsave(paste0("images/2023_MICs/",mic_date,"_MEC_",drug$drug[1],"_MIC.png"), 
-       drug_full_plot, 
-       width = 7.3, 
-       height = 8, 
-       units = "in", 
-       device = png, 
-       bg = "white",
-       dpi = 300)
+ggsave(paste0("images/2023_MICs/", mic_date, "_MEC_", drug$drug[1], "_MIC.png"),
+  drug_full_plot,
+  width = 7.3,
+  height = 8,
+  units = "in",
+  device = png,
+  bg = "white",
+  dpi = 300
+)
 ################################################################################
 # REDCap imports
 # Numeric values to text
@@ -226,38 +253,47 @@ drug_mic$concentration <- as.character(drug_mic$concentration)
 
 # Append > when needed
 drug_mic <- drug_mic %>%
-    mutate(concentration = case_when((mean_norm_OD > cutoff & concentration == max_concentration) ~ paste0(">", max_concentration),
-                                     .default = concentration))
+  mutate(concentration = case_when((mean_norm_OD > cutoff & concentration == max_concentration) ~ paste0(">", max_concentration),
+    .default = concentration
+  ))
 
-redcap_drug_value <- case_when(drug$drug[1] == "FLC" ~ 0,
-                               drug$drug[1] == "MCF" ~ 1,
-                               drug$drug[1] == "AMB" ~ 2)
+redcap_drug_value <- case_when(
+  drug$drug[1] == "FLC" ~ 0,
+  drug$drug[1] == "MCF" ~ 1,
+  drug$drug[1] == "AMB" ~ 2
+)
 
-redcap_drug_man <- case_when(drug$drug[1] == "FLC" ~ "Alfa Aesar",
-                             drug$drug[1] == "MCF" ~ "MedChemExpress",
-                             drug$drug[1] == "AMB" ~ "Chem-Impex Intl Inc")
+redcap_drug_man <- case_when(
+  drug$drug[1] == "FLC" ~ "Alfa Aesar",
+  drug$drug[1] == "MCF" ~ "MedChemExpress",
+  drug$drug[1] == "AMB" ~ "Chem-Impex Intl Inc"
+)
 
-redcap_drug_lot <- case_when(drug$drug[1] == "FLC" ~ "P04F029",
-                             drug$drug[1] == "MCF" ~ "22367",
-                             drug$drug[1] == "AMB" ~ "001448-180229")
+redcap_drug_lot <- case_when(
+  drug$drug[1] == "FLC" ~ "P04F029",
+  drug$drug[1] == "MCF" ~ "22367",
+  drug$drug[1] == "AMB" ~ "001448-180229"
+)
 
-redcap_drug_solvent <- case_when(drug$drug[1] == "FLC" ~ "EtOH",
-                                 drug$drug[1] == "MCF" ~ "DMSO",
-                                 drug$drug[1] == "AMB" ~ "DMSO")
+redcap_drug_solvent <- case_when(
+  drug$drug[1] == "FLC" ~ "EtOH",
+  drug$drug[1] == "MCF" ~ "DMSO",
+  drug$drug[1] == "AMB" ~ "DMSO"
+)
 
 # For each strain, create new record and send form
-for(i in c(1:length(drug_mic$strain))){
-    primary_id=drug_mic$strain[i]
-    
+for (i in c(1:length(drug_mic$strain))) {
+  primary_id <- drug_mic$strain[i]
+
   record <- c(
     primary_id = primary_id,
     redcap_repeat_instrument = "mic_results",
     redcap_repeat_instance = "new",
     drug = redcap_drug_value,
     mic_date = mic_date,
-    mic50 = as.character(drug_mic$concentration[drug_mic$strain==primary_id]),
+    mic50 = as.character(drug_mic$concentration[drug_mic$strain == primary_id]),
     mic_file_name = basename(mic_spreadsheet),
-    smg = as.character(drug_smg$mean_48[drug_smg$strain==primary_id]),
+    smg = as.character(drug_smg$mean_48[drug_smg$strain == primary_id]),
     smg_file_name = basename(smg_spreadsheet),
     mic_media = 1,
     mic_temp = 1,
@@ -268,18 +304,19 @@ for(i in c(1:length(drug_mic$strain))){
     mic_results_complete = 2
   )
 
-  result_data <- toJSON(list(as.list(record)), auto_unbox=TRUE)
+  result_data <- toJSON(list(as.list(record)), auto_unbox = TRUE)
 
-  formData <- list("token"=Sys.getenv("redcap_api_key"),
-                 content='record',
-                 action='import',
-                 format='json',
-                 type='flat',
-                 overwriteBehavior='normal',
-                 forceAutoNumber='false',
-                 data=result_data,
-                 returnContent='count',
-                 returnFormat='json'
+  formData <- list(
+    "token" = Sys.getenv("redcap_api_key"),
+    content = "record",
+    action = "import",
+    format = "json",
+    type = "flat",
+    overwriteBehavior = "normal",
+    forceAutoNumber = "false",
+    data = result_data,
+    returnContent = "count",
+    returnFormat = "json"
   )
   response <- httr::POST(api_url, body = formData, encode = "form")
   result <- httr::content(response)
--- redcap_reports/MIC_within_patient_changes.R
+++ redcap_reports/MIC_within_patient_changes.R
@@ -1,125 +1,154 @@
 ## ---------------------------
-## Purpose: Summarize and plot MEC isolate (EUCAST) MIC and SMG data by species 
+## Purpose: Summarize and plot MEC isolate (EUCAST) MIC and SMG data by species
 ## Author: Nancy Scott
 ## Email: scot0854@umn.edu
 ## ---------------------------
-options(scipen = 999) 
+options(scipen = 999)
 
 # Load packages
 library(patchwork)
-library(ggbeeswarm)
+library(gtools)
 library(data.table)
 
 source("MIC_data_summary.R")
 
-species_colors <- c("#88CCEE", "#999933", "#CC6677", "#44AA99", "#117733", "#332288",
-                    "#882255","#BBBBBB", "#AA4499", "#DDCC77", "black")
+species_colors <- c(
+  "#88CCEE", "#999933", "#CC6677", "#44AA99", "#117733", "#332288",
+  "#882255", "#BBBBBB", "#AA4499", "#DDCC77", "black"
+)
 
-species_colors <- species_colors %>% 
-    set_names(species_count$genus_species)
+species_colors <- species_colors %>%
+  set_names(species_count$genus_species)
 
-drugs <- as_labeller(c(fluconazole="Fluconazole", 
-                       micafungin="Micafungin",
-                       `amphotericin B` = "Amphotericin B"))
+drugs <- as_labeller(c(
+  fluconazole = "Fluconazole",
+  micafungin = "Micafungin",
+  `amphotericin B` = "Amphotericin B"
+))
 
 full_mic <- data.table::copy(mic_info)
 
 # Bin Etest reults with broth MIC levels
-full_mic <- full_mic %>% 
-  mutate(mic50 = replace(mic50, mic50==">32", "64")) %>% 
-  mutate(mic50 = replace(mic50, mic50=="160", "256"))
+full_mic <- full_mic %>%
+  mutate(mic50 = replace(mic50, mic50 == ">32", "64")) %>%
+  mutate(mic50 = replace(mic50, mic50 == "160", "256"))
 
-full_mic <- full_mic %>% 
-  mutate(mic90=replace(mic90, mic90=="0.023", "0.032"))
+full_mic <- full_mic %>%
+  mutate(mic90 = replace(mic90, mic90 == "0.023", "0.032"))
 
-full_mic <- full_mic %>% 
-  mutate(mic50 = case_when((drug=="micafungin" & genus_species=="C. parapsilosis" & mic50=="2") ~ ">1", 
-                         .default = mic50))
+full_mic <- full_mic %>%
+  mutate(mic50 = case_when((drug == "micafungin" & genus_species == "C. parapsilosis" & mic50 == "2") ~ ">1",
+    .default = mic50
+  ))
 
 # Drug-specific levels for plotting
 flc_levels <- c("0.5", "1", "2", "4", "8", "16", "32", "64", "128", "256")
 mcf_amb_levels <- c("0.016", "0.032", "0.064", "0.125", "0.256", "0.5", "1", ">1")
 
 # Drug-specific DFs
-mic_flc <- full_mic %>% 
-  filter(drug=="fluconazole")
+mic_flc <- full_mic %>%
+  filter(drug == "fluconazole")
 mic_flc$mic50 <- factor(mic_flc$mic50, levels = flc_levels)
 
-mic_mcf <- full_mic %>% 
-  filter(drug=="micafungin")
-mic_mcf$mic50 <- factor(mic_mcf$mic50, levels=mcf_amb_levels)
+mic_mcf <- full_mic %>%
+  filter(drug == "micafungin")
+mic_mcf$mic50 <- factor(mic_mcf$mic50, levels = mcf_amb_levels)
 
-mic_amb <- full_mic %>% 
-  filter(drug=="amphotericin B")
+mic_amb <- full_mic %>%
+  filter(drug == "amphotericin B")
 mic_amb$mic90 <- factor(mic_amb$mic90, levels = mcf_amb_levels)
 
 # Find the series with > 2-fold changes
-flc_step_diffs <- mic_flc %>% 
-  filter(!is.na(series_id)) %>% 
+flc_step_diffs <- mic_flc %>%
+  filter(!is.na(series_id)) %>%
   group_by(series_id) %>%
   summarize(diff = max(as.numeric(mic50), na.rm = T) - min(as.numeric(mic50), na.rm = T)) %>%
   filter(diff > 1)
 
-mcf_step_diffs <- mic_mcf %>% 
-  filter(!is.na(series_id)) %>% 
+mcf_step_diffs <- mic_mcf %>%
+  filter(!is.na(series_id)) %>%
   group_by(series_id) %>%
   summarize(diff = max(as.numeric(mic50), na.rm = T) - min(as.numeric(mic50), na.rm = T)) %>%
   filter(diff > 1)
 
-amb_step_diffs <- mic_amb %>% 
-  filter(!is.na(series_id)) %>% 
+amb_step_diffs <- mic_amb %>%
+  filter(!is.na(series_id)) %>%
   group_by(series_id) %>%
   summarize(diff = max(as.numeric(mic90), na.rm = T) - min(as.numeric(mic90), na.rm = T)) %>%
   filter(diff > 1)
 
-# Plot per drug, facet by patient
-flc_diff_plot <- mic_flc %>%  
-  filter(series_id %in% flc_step_diffs$series_id) %>%
-  mutate(relative_days = relative_days + 1) %>% 
-  ggplot(aes(x = relative_days, y = mic50, color = genus_species)) +
-  facet_wrap(.~patient_code) +
-  geom_beeswarm(cex = 2) +
-  scale_color_manual(values=species_colors, guide = "none") +
-  scale_x_continuous(limits = c(1,12), breaks = c(1,3,5,7,9,11)) +
-  theme_bw() +
-  theme(strip.text = element_text(color = "black")) + 
-  theme(axis.text = element_text(size = 8, color = "black")) +
-  theme(axis.title = element_text(size=11, color = "black")) +
-  ylab("Fluconazole MIC50") +
-  xlab(NULL)
+# Plot series AH and BB
+bb_flc <- mic_flc %>%
+  filter(series_id == "BB")
+bb_flc$sample_id <- c("1.1", "1.2", "1.4", "1.3", "2.4", "2.1", "2.2", "2.3")
 
-mcf_diff_plot <- mic_mcf %>% 
-  filter(series_id %in% mcf_step_diffs$series_id) %>%
-  mutate(relative_days = relative_days + 1) %>% 
-  ggplot(aes(x = relative_days, y = mic50, color = genus_species)) +
-  facet_wrap(.~patient_code) +
-  geom_beeswarm(cex = 2, show.legend = TRUE) +
-  scale_color_manual(values=species_colors, name = NULL, drop = FALSE) +
-  scale_x_continuous(limits = c(1,12), breaks = c(1,3,5,7,9,11)) +
+bb_flc_plot <- bb_flc %>%
+  ggplot(aes(x = sample_id, y = mic50, fill = genus_species)) +
+  geom_col() +
+  scale_fill_manual(values = species_colors, guide = "none") +
+  scale_y_discrete(drop = FALSE) +
   theme_bw() +
   theme(strip.text = element_text(color = "black")) +
-  theme(legend.text = element_text(color = "black", face = "italic")) +
   theme(axis.text = element_text(size = 8, color = "black")) +
-  theme(axis.title = element_text(size=11, color = "black")) +
-  ylab("Micafungin MIC50") +
+  theme(axis.title = element_text(size = 11, color = "black")) +
+  ylab(expression(paste("Fluconazole MIC50, ", mu, "g/mL"))) +
   xlab(NULL)
 
-amb_diff_plot <- mic_amb %>% 
-  filter(series_id %in% amb_step_diffs$series_id) %>%
-  mutate(relative_days = relative_days + 1) %>% 
-  ggplot(aes(x = relative_days, y = mic90, color = genus_species)) +
-  facet_wrap(.~patient_code) +
-  geom_beeswarm(cex = 2) +
-  scale_color_manual(values=species_colors, guide = "none") +
-  scale_x_continuous(limits = c(1,12), breaks = c(1,3,5,7,9,11)) +
+bb_amb <- mic_amb %>%
+  filter(series_id == "BB")
+bb_amb$sample_id <- c("1.1", "1.2", "1.4", "1.3", "2.4", "2.1", "2.2", "2.3")
+
+bb_amb_plot <- bb_amb %>%
+  ggplot(aes(x = sample_id, y = mic90, fill = genus_species)) +
+  geom_col() +
+  scale_fill_manual(values = species_colors, guide = "none") +
+  scale_y_discrete(drop = FALSE) +
   theme_bw() +
-  theme(strip.text = element_text(color = "black")) + 
+  theme(strip.text = element_text(color = "black")) +
   theme(axis.text = element_text(size = 8, color = "black")) +
-  theme(axis.title = element_text(size=11, color = "black")) +
-  ylab("Amphotericin B MIC90") +
-  xlab("Collection day")
+  theme(axis.title = element_text(size = 11, color = "black")) +
+  ylab(expression(paste("Amphotericin B MIC90, ", mu, "g/mL"))) +
+  xlab(NULL)
 
-combined_diffs <- (flc_diff_plot/(mcf_diff_plot + plot_layout(ncol = 3, nrow = 1))/amb_diff_plot) +
-  plot_layout(heights = c(2,1,2), guides = "collect") + plot_annotation(tag_levels = "A")
+ah_mcf <- mic_mcf %>%
+  filter(series_id == "AH")
+ah_mcf$sample_id <- c("1.1", "1.2", "1.3", "1.4", "2.1", "2.2", "3.2", "4.1", "3.1", "4.2", "5.1", "5.2", "6.1", "6.2", "7.1", "7.2", "8.1", "8.2", "9.1", "11.1")
 
-ggsave("MEC_within_patient_MIC_differences.pdf", combined_diffs, width = 7, height = 9, units = "in")
+level_order <- mixedsort(ah_mcf$sample_id)
+
+ah_mcf_plot <- ah_mcf %>%
+  ggplot(aes(x = sample_id, y = mic50, fill = genus_species)) +
+  geom_col(aes(x = factor(sample_id, level = level_order))) +
+  scale_fill_manual(values = species_colors, guide = "none") +
+  scale_y_discrete(drop = FALSE) +
+  theme_bw() +
+  theme(strip.text = element_text(color = "black")) +
+  theme(axis.text.y = element_text(size = 8, color = "black")) +
+  theme(axis.text.x = element_text(size = 6, color = "black")) +
+  theme(axis.title = element_text(size = 11, color = "black")) +
+  ylab(expression(paste("Micafungin MIC50, ", mu, "g/mL"))) +
+  xlab(NULL)
+
+ah_amb <- mic_amb %>%
+  filter(series_id == "AH")
+ah_amb$sample_id <- ah_mcf$sample_id
+
+ah_amb_plot <- ah_amb %>%
+  ggplot(aes(x = sample_id, y = mic90, fill = genus_species)) +
+  geom_col(aes(x = factor(sample_id, level = level_order))) +
+  scale_fill_manual(values = species_colors, guide = "none") +
+  scale_y_discrete(drop = FALSE) +
+  theme_bw() +
+  theme(strip.text = element_text(color = "black")) +
+  theme(axis.text.y = element_text(size = 8, color = "black")) +
+  theme(axis.text.x = element_text(size = 6, color = "black")) +
+  theme(axis.title = element_text(size = 11, color = "black")) +
+  ylab(expression(paste("Amphotericin B MIC90, ", mu, "g/mL"))) +
+  xlab(NULL)
+
+bb_combined <- bb_flc_plot + bb_amb_plot
+ah_combined <- ah_mcf_plot + ah_amb_plot
+
+combined_series_diffs <- bb_combined / ah_combined + plot_annotation(tag_levels = list(c("A", "", "B", "")))
+
+ggsave("MEC_serial_MIC_differences.tiff", combined_series_diffs, width = 8, height = 6, units = "in", dpi = 320)
